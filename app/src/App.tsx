f3b47758f8be51ffa4905f7cf893db3f9f581996
i15317 <30828280+i15317@users.noreply.github.com>
2021-07-28 22:30:51 +0900

    ルーティングの実装

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+
+
+const Toppage = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
+}
+const PublicPage = () => {
+    return (
+        <div className="name-page">
+            public page
+        </div>
+    );
+}
+
+
+const TodoPage = () => {
+    return (
+        <div>
+            This is todo page!
+        </div>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/" exact component={Toppage}/>
+                    <Route path="/public" exact component={PublicPage}/>
+                    <Route path="/edit/todo" exact component={TodoPage}/>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ app/src/components/Todo.tsx
@@ -0,0 +1,17 @@
+interface OwnProps {
+    task: string
+    is_finished: boolean
+}
+type Props = OwnProps
+export const Todo: React.FC<Props> = props => {
+    return (
+        <li className="todo stack-small">
+            <div className="c-cb">
+                <input id="todo-0" type="checkbox" defaultChecked={false}/>
+                <label className="todo-label" htmlFor="todo-0">
+                    {props.task}
+                </label>
+            </div>
+        </li>
+    );
+}
