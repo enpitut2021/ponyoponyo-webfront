dae79e3fbe0d70eda8d384e7dbd8f26848011384
xiaoyunyi <s1913024@s.tsukuba.ac.jp>
2021-07-30 14:13:27 +0900

    correct version 14:13

--- app/src/state/Task.ts
+++ app/src/state/Task.ts
@@ -1,10 +1,19 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
+
+export type Task = {
+    task: string,
+    deadline: string,
+    is_finished: boolean,
+
+}
+
 //モデル
 export type TaskState = {
     task: string,
     deadline: string,
     is_finished: boolean,
+    input_value: string,
 }
 
 //モデルの初期値
@@ -12,6 +21,7 @@ export const initialState: TaskState = {
     task: "数学のレポートを滅する",
     is_finished: false,
     deadline: "2021/7/28",
+    input_value: "",
 }
 
 //モデルから値の保存場所（State）を作る
@@ -26,6 +36,9 @@ const taskSlice = createSlice({
         //チェックボックスにチェックが付かない・・・false
         notifyChangeTaskState: (state, action: PayloadAction<boolean>) => ({
             ...state, is_finished: action.payload
+         }), 
+        notifyChangeInputValue: (state, action: PayloadAction<string>) => ({
+            ...state, input_value: action.payload
         })
     }
 })
